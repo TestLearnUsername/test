9bdaa227799975a7eed87fc798c9ba723862786c
TestLearnUsername <126953999+TestLearnUsername@users.noreply.github.com>
2023-03-04 18:17:17 +0100

    key from ngrok

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World from Java file changes webhook dodanie klucza z jenkinsa do githuba usuniecie ssh z gityy");
+		System.out.println("Hello World from Java file changes webhook dodanie klucza z ngrok do githuba alow ssh");
 	}
 }
