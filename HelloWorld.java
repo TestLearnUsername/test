ea79e7efab98a44982346640de7190cc87cb4f3a
TestLearnUsername <126953999+TestLearnUsername@users.noreply.github.com>
2023-03-04 19:18:08 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World from Java file changes whgfhebhook djhhgfhfghghgjhgodanie bmbnmbnmm klucza z ngrok do githuba hhhdisable endpoint odpowiedni hhhhhssh");
+		System.out.println("W KOŃCU DZIAŁA");
 	}
 }
